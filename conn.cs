9272604078c6401b18d552705a8cb849e0ca6063
Shashidhar <shashidhar.gandi@gmail.com>
2022-09-06 14:48:05 +0530

    Update conn.cs

--- conn.cs
+++ conn.cs
@@ -1,1 +1,23 @@
 using System.Data.SqlClient;
+class Employee{
+  
+}
+class DBApp{
+  
+  pulic static void EstConn(){
+    string str = @"server=localhost;database=userdb;trusted_connection=true;";
+    var conn = new SqlConnection(str);
+    conn.Open();
+    Console.WriteLine("Connection Established");
+  }
+  
+  public static void CloseConn(){
+    conn.Close();
+    conn.Dispose();
+  }
+  pulbic static void Main(string[] args){
+    EstConn();
+    CloseConn();
+    
+  }
+}
